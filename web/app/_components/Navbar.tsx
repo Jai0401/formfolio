30e737851a739586a15032ed40e88ae162286255
Jai0401 <21cs3025@rgipt.ac.in>
2024-09-07 23:20:43 +0530

    initial upload

---
+++ web/app/_components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export default function Navbar() {
+    return(
+        <div className="py-2 text-black">
+            <h1 className="text-center text-3xl font-semibold tracking-tighter ">Formfolio</h1>
+        </div>
+    )
+}
---
+++ web/app/page.tsx
@@ -0,0 +1,12 @@
+import Form from "./_components/Form";
+import Navbar from "./_components/Navbar";
+
+
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+    <Form />
+    </>
+  );
+}
