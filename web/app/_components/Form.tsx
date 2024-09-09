b84dc78dae56e407e18b2806ef8baf439362496e
Jai0401 <21cs3025@rgipt.ac.in>
2024-09-09 22:39:32 +0530

    Refactor form layout and add input fields for name and description

--- web/app/_components/Form.tsx
+++ web/app/_components/Form.tsx
@@ -1,20 +1,29 @@
 export default function Form(){
     return (
-        <div className="py-2 flex justify-center pt-10">
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        <div className="py-4 flex justify-center items-center pt-10">
+            <form className="border-black border-2 rounded-lg p-8 sm:w-[500px]">
+                <div className="mb-3 flex justify-center">
+                    <div>
+                        <label htmlFor="inputName" className="form-label font-medium tracking-tighter">Name</label><br></br>
+                        <input type="text" 
+                        className="form-control px-2 py-1 rounded-md border-2 border-black/10 sm:w-96" 
+                        id="input Name" 
+                        placeholder="Enter your name"
+                        aria-describedby="emailHelp"/>
+                    </div>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                <div className="mb-3 flex justify-center">
+                    <div>
+                        <label htmlFor="description" className="form-label tracking-tighter font-medium">Brief Description</label><br></br>
+                        <textarea
+                        className="form-control px-2 py-1 rounded-md border-2 border-black/10 h-40 sm:w-96" 
+                        placeholder="I am a Computer Science student at Rajiv Gandhi Institute of Petroleum Technology. My passion lies in Machine Learning and Web Development, and during my spare time, I actively participate in open-source projects..."
+                        id="description"/>
+                    </div>
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                <div className="flex justify-end">
+                    <button type="submit" className="btn btn-primary bg-black rounded-lg px-4 py-1 hover:bg-black/80 font-semibold text-white">Submit</button>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         </div>
     )
