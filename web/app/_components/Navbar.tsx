b2625757a6473de4e22f73eae22a01e4cb0c5d58
Jai0401 <21cs3025@rgipt.ac.in>
2024-09-11 21:36:38 +0530

    Update Navbar component with introductory text

--- web/app/_components/Navbar.tsx
+++ web/app/_components/Navbar.tsx
@@ -3,6 +3,7 @@ export default function Navbar() {
     return(
         <div className="py-2 text-black">
             <h1 className="text-center text-3xl font-semibold tracking-tighter ">Formfolio</h1>
+            <p className="text-center text-lg font-semibold tracking-tighter">Fill the form below and get your portfolio site instantly</p>
         </div>
     )
 }
